94fc27bb44032595158fd971737014f64a06d57b
Torben <tgotowik@protonmail.com.com>
2025-09-16 19:13:46 +0200

    add spielestyler mode; import-csv in spielestyler format

--- flyff-character-ranking-plotter.py
+++ flyff-character-ranking-plotter.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import json
+import re
+import argparse
 
 DEBUG = False
 
@@ -17,8 +20,8 @@ SERVERS = {
     "Totemia": "9",
     "Burudeng": "11",
     "Flarine": "20",
-    "Japanese": "23",
-    "Chinese": "25"
+    "リシス": "23",
+    "獨眼蝙蝠": "25"
 }
     
 def get_server_info(server_id):
@@ -72,13 +75,14 @@ def get_server_info(server_id):
         print(class_counts)
     return level, class_counts
 
-def new_plot(df):
+def new_plot(df, date=TODAY, path_spielestyler=False):
     # create figure and axes
     fig, ax = plt.subplots(figsize=(12, 8))
     
     # define color for each class
     colors = ['#ff6767', '#db3131', '#737aff', '#474dc2', '#49de72', '#6bc16c', '#945eff', '#5500ff']
 
+    # plt.rcParams["font.family"] = "Arial Unicode MS" # TODO: Japanese and Chinese fonts
     # create plot
     df.plot(kind='bar', stacked=True, ax=ax, color=colors)
     
@@ -102,31 +106,106 @@ def new_plot(df):
             ax.text(x, y, str(int(height)), ha='center', va='center', fontsize=10, color='white')
 
     # Save plots to data
-    plt.savefig(f'plots/{TODAY}-flyff-universe-character-ranking-plot.png', bbox_inches='tight')
-    plt.show()
+    if path_spielestyler == True:
+        plt.savefig(f'plots/spielestyler-plots/{date}-flyff-universe-character-ranking-plot.png', bbox_inches='tight')
+    else:
+        plt.savefig(f'plots/{date}-flyff-universe-character-ranking-plot.png', bbox_inches='tight')
+    
+    if DEBUG == True:
+        plt.show()
+
+def parse_spielestyler_file(path_csv):
+    json_data = dict()
+
+    classes_rows_number = {
+        0: "Ringmaster",
+        1: "Psykeeper",
+        2: "Jester",
+        3: "Billposter",
+        4: "Elementor",
+        5: "Blade",
+        6: "Ranger",
+        7: "Knight"
+    }
+    
+    date_pattern = re.compile(r'^\d{2}\.\d{2}\.\d{4}')
+
+    with open(path_csv, "r", encoding="utf-8-sig") as f:  # <- hier encoding setzen
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            
+            # If "Stand" or date_pattern was found get date
+            if line.startswith("Stand ") or date_pattern.match(line):
+                if line.startswith("Stand "):
+                    date = line.split()[1].split(";")[0]
+                elif date_pattern.match(line):
+                    date = line.split(";")[0]
+                if date not in json_data:
+                    json_data[date] = {}
+            if line.split(";")[0] in SERVERS:
+                server = line.split(";")[0]
+                for i, col in enumerate(line.split(";")[1:]):
+                    if i < 8:
+                        if classes_rows_number[i] not in json_data[date]:
+                            json_data[date][classes_rows_number[i]] = {}
+                        json_data[date][classes_rows_number[i]][server] = col
+    return json_data
 
 if __name__ == "__main__":
-    mapped_data = {}
+    parser = argparse.ArgumentParser(
+        description="Tool to plot Flyff Universe Character Ranking."
+    )
+    parser.add_argument(
+        "--new-plot",
+        action="store_true",
+        help="Create new plot with latest data."
+    )
+    parser.add_argument(
+        "--import-csv",
+        type=str,
+        help="Import Spielestyler data."
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Turn on debugging."
+    )
 
-    # get class and level data each server
-    for server in SERVERS:
-        level_data, class_data = get_server_info(SERVERS[server])
+    args = parser.parse_args()
+    
+    if args.debug:
+        DEBUG = True
 
-        # error when no data was fetched
-        if level_data is None or class_data is None:
-            print(f"Error: Did not fetch data for: {server}")
-            continue 
+    if args.new_plot:
+        mapped_data = {}
+        
+        # get class and level data each server
+        for server in SERVERS:
+            level_data, class_data = get_server_info(SERVERS[server])
 
-        # map server to classes
-        for class_name, class_value in class_data.items():
-            if class_name not in mapped_data:
-                mapped_data[class_name] = {}
-            mapped_data[class_name][server] = class_value
+            # error when no data was fetched
+            if level_data is None or class_data is None:
+                print(f"Error: Did not fetch data for: {server}")
+                continue 
 
-    # make panda frame
-    df = pd.DataFrame(mapped_data)
+            # map server to classes
+            for class_name, class_value in class_data.items():
+                if class_name not in mapped_data:
+                    mapped_data[class_name] = {}
+                mapped_data[class_name][server] = class_value
+        
+        # make panda frame
+        df = pd.DataFrame(mapped_data)
 
-    if DEBUG == True:
-        print(df)
+        if DEBUG == True:
+            print(df)
 
-    new_plot(df)
+        new_plot(df)
+    if args.import_csv:
+        imported_data = parse_spielestyler_file(args.import_csv)
+        
+        for date in imported_data:
+            df = pd.DataFrame(imported_data[date]).astype(int)
+            new_plot(df, date, True)
